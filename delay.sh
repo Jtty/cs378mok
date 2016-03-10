659f7797586bf2f922ceddbb4a429559e1ca61b4
BXRBudda <rajanbose@utexas.edu>
2016-03-10 16:20:11 -0600

    added 'display' parameter to display script

--- delay.sh
+++ delay.sh
@@ -10,7 +10,10 @@ case $1 in
 	remove)
 		sudo tc qdisc del dev lo root netem
 		;;
+	display)
+		sudo ping 127.0.0.1
+		;;
 	*)
-		echo -e "Usage:\n\tadd <num+ms>\n\tchange <num+ms>\n\tremove\n\t*NOTE: must be a sudoer. Don't need to run script with sudo as it is included in command"
+		echo -e "Usage:\n\tadd <num+ms>\n\tchange <num+ms>\n\tremove\n\tdisplay\n\t*NOTE: must be a sudoer. Don't need to run script with sudo as it is included in command"
 		;;
 esac
