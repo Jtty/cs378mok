aaca986a85bbe1f80bc2064a096704f794d58c3b
Marc Aldrich <marcaldrich@gmail.com>
2016-03-17 11:43:36 -0500

    Updated build script to automatically detect compile error. This stops the build and continues

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,8 +3,8 @@
 pid_java=`ps | grep java | head -c 5`
 
 # Kill previous server
-if [ -z $pid_java ]
-	then
+if [ -z $pid_java ] 
+then
 		echo "Old server not found. Skipping the kill"
 else
 		echo "Found exisitng server instance. Killing it."
@@ -14,7 +14,11 @@ fi
 # Rebuild codebase
 echo "Rebuilding all code: javac *.java. There is normally one warning"
 javac *.java -Xlint:deprecation
-
+if [ $0 ]
+then 
+    echo "There was a compilation problem. Not starting server or applet"
+    exit 1
+fi
 
 
 # Restart server
